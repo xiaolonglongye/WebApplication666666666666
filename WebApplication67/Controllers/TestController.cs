c1c86de983bc4b1219efaf9eb942e33ec5134e14
小龙爷 <59511652@qq.com>
2020-03-24 18:43:19 +0800

    修改的版本

---
+++ WebApplication67/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication67.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Test()
+        {
+            return View();
+        }
+        public ActionResult Demo()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication67/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication67.MvcApplication" Language="C#" %>
